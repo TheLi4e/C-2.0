a2a5ac42a11f90702bf407e82c266df545a2c737
TheLi4e <li4e99@gmail.com>
2023-10-15 15:10:36 +0200

    HW2

--- C#2.0/Program.cs
+++ C#2.0/Program.cs
@@ -1,62 +1,31 @@
-﻿using System.Collections.Immutable;
-using System.Text;
-using C_2._0;
-if (args.Length > 1)
-{
-    List<string> list = args.ToList();
+﻿using C_2._0;
 
-    while (list.Contains("*") || list.Contains("/"))
-    {
-        list = MathForString.CalculateStringMultAndDiv(list);
-    }
-    while (list.Contains("+") || list.Contains("-"))
-    {
-        list = MathForString.CalculateStringSumAndMinus(list);
-    }
+//Создаем двумерный массив
+int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
 
-    Console.WriteLine($"Результат: {list[0]}");
-}
-
-int[,] a =
-        {
-            { 7, 3, 2 },
-            { 4, 9, 6 },
-            { 1, 8, 2 }
-        };
-int p = 0;
-int[] b = new int[a.GetLength(0) * a.GetLength(1)];
-
-for (int j = 0; j < a.GetLength(0); j++)
-{
-    for (int k = 0; k < a.GetLength(1); k++)
-    {
-        b[p] = a[j, k];
-        p++;
-    }
-}
-p = 0;
-Array.Sort(b);
-
-p = 0;
-for (int j = 0; j < a.GetLength(0); j++)
-{
-    for (int k = 0; k < a.GetLength(1); k++)
-    {
-        a[j, k] = b[p];
-        p++;
-    }
-}
-
-for (int i = 0; i < a.GetLength(0); i++)
+//Выводим на печать заданный массив
+for (int i = 0; i <a.GetLength(0); i++)
 {
     for (int j = 0; j < a.GetLength(1); j++)
     {
-        Console.Write(a[i, j]);
+        Console.Write($"{a[i, j]}\t");
+        
     }
     Console.WriteLine();
 }
 
-Console.ReadKey();
+Console.WriteLine();
 
+//Сортируем двумерный массив с помощью класса Arrays
+var sortedArray = Arrays.Task1(Arrays.ConcatArray(a));
 
+//Выводим на печать отсортированный массив
+for (int i = 0; i < sortedArray.GetLength(0); i++)
+{
+    for (int j = 0; j < sortedArray.GetLength(1); j++)
+    {
+        Console.Write($"{sortedArray[i, j]}\t");
 
+    }
+    Console.WriteLine();
+}
