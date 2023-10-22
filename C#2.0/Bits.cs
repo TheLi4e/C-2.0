4b088d1d7854e233fea9231fffca692871d02414
TheLi4e <li4e99@gmail.com>
2023-10-22 16:10:21 +0200

    Add HW4

---
+++ C#2.0/Bits.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace C_2._0
+{
+    //Реализуйте операторы неявного приведения из long,int,byt в Bits.
+    internal class Bits
+    {
+        byte Value { get; set; }
+        public Bits(byte bits) { Value = bits; }
+
+        public static implicit operator byte(Bits bits)
+        {
+            return new Bits(Convert.ToByte(bits)); 
+        }
+
+        public static implicit operator long(Bits bits)
+        {
+            return new Bits(Convert.ToByte(bits)); 
+        }
+
+        public static implicit operator int(Bits bits)
+        {
+            return new Bits(Convert.ToByte((bits)));
+        }
+    }
+}
+
