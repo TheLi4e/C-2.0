5c162f3ae24980854d2bb8b1c8b9b29b5bf46bd2
TheLi4e <li4e99@gmail.com>
2023-10-22 16:44:02 +0200

    Added HW5

--- C#2.0/Program.cs
+++ C#2.0/Program.cs
@@ -6,20 +6,6 @@ internal class Program
     private static void Main(string[] args)
     {
 
-        const int WINDOW_HEGHT = 11;
-        const int WINDOW_WIDTH = 120;
-        
-        GUI.DrawWindow(0, 0, WINDOW_WIDTH, WINDOW_HEGHT);
-
-        var man = new Man("Alesha", "Korobov", Gender.male, DateTime.Now);
-        var woman = new Woman("Ksenia", "Alehina", Gender.female, DateTime.Now);
-        
-        woman.AddHusband(man);
-        man.AddWife(woman);
-        
-        Console.SetCursorPosition(2, 1);
-        Console.WriteLine(woman.PrintHusband());
-        Console.SetCursorPosition(2, 6);
-        Console.WriteLine(man.PrintWife());
+        Console.WriteLine("Найдено выходов: " + new Labirinth().HasExix(1, 3));
     }
 }
