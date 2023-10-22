f368f97f6fc8648a2703632649426aa3141a9c29
TheLi4e <li4e99@gmail.com>
2023-10-22 19:11:04 +0200

    Корректировка ДЗ

--- C#2.0/Bits.cs
+++ C#2.0/Bits.cs
@@ -4,28 +4,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace C_2._0
 {
     //Реализуйте операторы неявного приведения из long,int,byt в Bits.
-    internal class Bits
+    internal class Bits : IBits
     {
-        byte Value { get; set; }
-        public Bits(byte bits) { Value = bits; }
+        public long Value { get; set; } = 0;
+        private int SizeOfValue { get; set; }
 
-        public static implicit operator byte(Bits bits)
+        public Bits(byte b)
         {
-            return new Bits(Convert.ToByte(bits)); 
+            Value = b;
+            SizeOfValue = sizeof(byte) * 8;
         }
 
-        public static implicit operator long(Bits bits)
+        public Bits(short b)
         {
-            return new Bits(Convert.ToByte(bits)); 
+            Value = b;
+            SizeOfValue = sizeof(short) * 8;
         }
 
-        public static implicit operator int(Bits bits)
+        public Bits(int b)
         {
-            return new Bits(Convert.ToByte((bits)));
+            Value = b;
+            SizeOfValue = sizeof(int) * 8;
+        }
+
+        public Bits(long b)
+        {
+            this.Value = b;
+            SizeOfValue = sizeof(long) * 8;
+        }
+        public static implicit operator long(Bits b) => b.Value;
+        public static implicit operator Bits(int b) => new Bits(b);
+        public static implicit operator Bits(byte b) => new Bits(b);
+        public static implicit operator Bits(short b) => new Bits(b);
+        public static implicit operator Bits(long b) => new Bits(b);
+
+        public bool GetBit(int i)
+        {
+            if (i > SizeOfValue || i < 0)
+            {
+                throw new Exception("Значение бита дожнобыть в пределах от 0 до 8");
+            }
+            return ((Value >> i) & 1) == 1;
+        }
+
+        public void SetBit(bool bit, int index)
+        {
+            if (index > SizeOfValue || index < 0) return;
+            if (bit == true)
+                Value = (byte)(Value | (1 << index));
+            else
+            {
+                var mask = (byte)(1 << index);
+                mask = (byte)(0xff ^ mask);
+                Value &= (byte)(Value & mask);
+            }
+        }
+
+        public void PrintBit()
+        {
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    sb.Append(GetBit(i) ? 1 : 0);
+
+                }
+                sb.ToString().Reverse();
+                Console.WriteLine(sb);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
---
+++ C#2.0/IBits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_2._0
+{
+    internal interface IBits
+    {
+        public bool GetBit(int i);
+        public void SetBit(bool bit, int index);
+    }
+}
