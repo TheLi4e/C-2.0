5dcdbd6eca68d3e42e7e53d0f01c528705b68c40
TheLi4e <li4e99@gmail.com>
2023-11-27 18:28:45 +0200

    Lesson10

--- C#2.0/Program.cs
+++ C#2.0/Program.cs
@@ -3,38 +3,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //Доработайте программу калькулятор реализовав выбор действий и вывод результатов на экран в цикле так,
-        //чтобы калькулятор мог работать до тех пор пока пользователь не нажмет отмена или введёт пустую строку.
-
-       Calc calc = new();
-        calc.handler += SubscribeMyEvent;
-        string exitCommand = String.Empty;
-        string operation = String.Empty;
-        string command = String.Empty;
-        double num = 0;
-
-        Console.WriteLine("Добро пожаловать в калькулятор.");
-
-        while (true)
-        {
-
-            calc.Run();
-            Console.WriteLine("Для завершения работы введите: " +
-                "отмена или поставьте пробел и нажмите Enter\n" +
-                "Для продолжения работы нажмите Enter");
-            exitCommand = Console.ReadLine();
-            if (exitCommand.ToLower().Equals("отмена") || exitCommand.ToLower().Equals(" "))
-            {
-                Console.WriteLine("Для завершения работы нажмите любую кнопку");
-                Console.ReadKey(true);
-                break;
-            }
-            Console.Clear();
-        }
-    }
-    public static void SubscribeMyEvent(object? sender, EventArgs e)
-    {
-        if (sender is Calc)
-            Console.WriteLine(((Calc)sender)._result);
+       Lesson10 lesson10 = new Lesson10();
+       lesson10.MyCopyFile(@"D:\GeekBrainsHomework\С#2.0\C#2.0\", "1.txt", "1.txt");
     }
 } 
